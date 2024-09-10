7f67755376df17379563d0bccaa3a4adaa033070
mm24005 <mm24005@esfe.agape.edu.sv>
2024-09-10 10:43:51 -0600

    Actualizar productos existentes

--- NDMM20240910_GUIA_1/Program.cs
+++ NDMM20240910_GUIA_1/Program.cs
@@ -36,6 +36,19 @@ app.MapPost("/products", (Product product) =>
     return Results.Ok();
 });
 
+// actualizar un producto existente
+app.MapPut("/products/{id}", (int id, Product product) =>
+{
+    var existingProduct = products.FirstOrDefault(p => p.Id == id);
+    if (existingProduct != null)
+    {
+        existingProduct.Name = product.Name;
+        existingProduct.Price = product.Price;
+        return Results.Ok();
+    }
+    return Results.NotFound();
+});
+
 app.UseAuthorization();
 
 app.MapControllers();
